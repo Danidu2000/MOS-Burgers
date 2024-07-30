f6c5f0a969e43e464182c60e5320c4505e088f1d
Danidu2000 <daniduparamuditha2000@gmail.com>
2024-07-31 00:35:16 +0530

    Correct the error of not loading css and imgs

--- items.js
+++ items.js
@@ -2,7 +2,7 @@ function B1001() {
     addBurger({
         id: 'B1001',
         name: 'Classic Burger (Large)',
-        imgSrc: '/image/Bueger 1/Classic Burger (Large) .png',
+        imgSrc: './image/Bueger 1/Classic Burger (Large) .png',
         unitPrice: 750.00,
         discount: '-',
         netPrice: 750.00
@@ -13,7 +13,7 @@ function B1002() {
     addBurger({
         id: 'B1002',
         name: 'Classic Burger (Regular)',
-        imgSrc: '/image/Bueger 1/Classic Burger (Regular).png',
+        imgSrc: './image/Bueger 1/Classic Burger (Regular).png',
         unitPrice: 1500.00,
         discount: '15%',
         netPrice: 1275.00
@@ -24,7 +24,7 @@ function B1003() {
     addBurger({
         id: 'B1003',
         name: 'Turkey Burger',
-        imgSrc: '/image/Bueger 1/Turkey Burger.png',
+        imgSrc: './image/Bueger 1/Turkey Burger.png',
         unitPrice: 1600.00,
         discount: '-',
         netPrice: 1600.00
@@ -35,7 +35,7 @@ function B1004() {
     addBurger({
         id: 'B1004',
         name: 'Chicken Burger (Large)',
-        imgSrc: '/image/Bueger 1/Chicken Burger (Large).png',
+        imgSrc: './image/Bueger 1/Chicken Burger (Large).png',
         unitPrice: 1400.00,
         discount: '-',
         netPrice: 1400.00
@@ -46,7 +46,7 @@ function B1005() {
     addBurger({
         id: 'B1005',
         name: 'Chicken Burger (Regular)',
-        imgSrc: '/image/Bueger 1/Chicken Burger (Regular).png',
+        imgSrc: './image/Bueger 1/Chicken Burger (Regular).png',
         unitPrice: 1400.00,
         discount: '20%',
         netPrice: 1120.00
@@ -57,7 +57,7 @@ function B1006() {
     addBurger({
         id: 'B1006',
         name: 'Cheese Burger (Large)',
-        imgSrc: '/image/Burger 2/Cheese Burger (Large).png',
+        imgSrc: './image/Burger 2/Cheese Burger (Large).png',
         unitPrice: 1000.00,
         discount: '-',
         netPrice: 1000.00
@@ -68,7 +68,7 @@ function B1007() {
     addBurger({
         id: 'B1007',
         name: 'Cheese Burger (Regular)',
-        imgSrc: '/image/Burger 2/Cheese Burger (Regular).png',
+        imgSrc: './image/Burger 2/Cheese Burger (Regular).png',
         unitPrice: 600.00,
         discount: '-',
         netPrice: 600.00
@@ -79,7 +79,7 @@ function B1008() {
     addBurger({
         id: 'B1008',
         name: 'Bacon Burger',
-        imgSrc: '/image/Burger 2/Bacon Burger.png',
+        imgSrc: './image/Burger 2/Bacon Burger.png',
         unitPrice: 650.00,
         discount: '15%',
         netPrice: 552.50
@@ -90,7 +90,7 @@ function B1009() {
     addBurger({
         id: 'B1009',
         name: 'Shawarma Burger',
-        imgSrc: '/image/Burger 2/Shawarma Burger.png',
+        imgSrc: './image/Burger 2/Shawarma Burger.png',
         unitPrice: 800.00,
         discount: '-',
         netPrice: 800.00
@@ -101,7 +101,7 @@ function B1010() {
     addBurger({
         id: 'B1010',
         name: 'Olive Burger',
-        imgSrc: '/image/Burger 2/Olive Burger.png',
+        imgSrc: './image/Burger 2/Olive Burger.png',
         unitPrice: 1800.00,
         discount: '-',
         netPrice: 1800.00
@@ -112,7 +112,7 @@ function B1012() {
     addBurger({
         id: 'B1012',
         name: 'Double-Cheese Burger',
-        imgSrc: '/image/Burger 3/Double-Cheese Burger.png',
+        imgSrc: './image/Burger 3/Double-Cheese Burger.png',
         unitPrice: 1250.00,
         discount: '20%',
         netPrice: 1000.00
@@ -123,7 +123,7 @@ function B1013() {
     addBurger({
         id: 'B1013',
         name: 'Crispy Chicken Burger (Regular)',
-        imgSrc: '/image/Burger 3/Crispy Chicken Burger (Regular).png',
+        imgSrc: './image/Burger 3/Crispy Chicken Burger (Regular).png',
         unitPrice: 1200.00,
         discount: '-',
         netPrice: 1200.00
@@ -134,7 +134,7 @@ function B1014() {
     addBurger({
         id: 'B1014',
         name: 'Crispy Chicken Burger (Large)',
-        imgSrc: '/image/Burger 3/Crispy Chicken Burger (Large).png',
+        imgSrc: './image/Burger 3/Crispy Chicken Burger (Large).png',
         unitPrice: 1600.00,
         discount: '10%',
         netPrice: 1440.00
@@ -145,7 +145,7 @@ function B1015() {
     addBurger({
         id: 'B1015',
         name: 'Paneer Burger',
-        imgSrc: '/image/Burger 3/Paneer Burger.png',
+        imgSrc: './image/Burger 3/Paneer Burger.png',
         unitPrice: 900.00,
         discount: '-',
         netPrice: 900.00
@@ -156,7 +156,7 @@ function B1016() {
     addBurger({
         id: 'B1016',
         name: 'Crispy Chicken Submarine (Large)',
-        imgSrc: '/image/Submarine 1/Crispy Chicken Submarine (Large).png',
+        imgSrc: './image/Submarine 1/Crispy Chicken Submarine (Large).png',
         unitPrice: 2000.00,
         discount: '-',
         netPrice: 2000.00
@@ -167,7 +167,7 @@ function B1017() {
     addBurger({
         id: 'B1017',
         name: 'Crispy Chicken Submarine (Regular)',
-        imgSrc: '/image/Submarine 1/Crispy Chicken Submarine (Regular).png',
+        imgSrc: './image/Submarine 1/Crispy Chicken Submarine (Regular).png',
         unitPrice: 1500.00,
         discount: '-',
         netPrice: 1500.00
@@ -178,7 +178,7 @@ function B1018() {
     addBurger({
         id: 'B1018',
         name: 'Chicken Submarine (Large)',
-        imgSrc: '/image/Submarine 1/Chicken Submarine (Large) .png',
+        imgSrc: './image/Submarine 1/Chicken Submarine (Large) .png',
         unitPrice: 1800.00,
         discount: '3%',
         netPrice: 1746.00
@@ -189,7 +189,7 @@ function B1019() {
     addBurger({
         id: 'B1019',
         name: 'Chicken Submarine (Regular)',
-        imgSrc: '/image/Submarine 1/Chicken Submarine (Regular).png',
+        imgSrc: './image/Submarine 1/Chicken Submarine (Regular).png',
         unitPrice: 1400.00,
         discount: '-',
         netPrice: 1400.00
@@ -200,7 +200,7 @@ function B1020() {
     addBurger({
         id: 'B1020',
         name: 'Grinder Submarine',
-        imgSrc: '/image/Submarine 1/Grinder Submarine.png',
+        imgSrc: './image/Submarine 1/Grinder Submarine.png',
         unitPrice: 2300.00,
         discount: '-',
         netPrice: 2300.00
@@ -211,7 +211,7 @@ function B1021() {
     addBurger({
         id: 'B1021',
         name: 'Cheese Submarine',
-        imgSrc: '/image/Submarine 2/Cheese Submarine.png',
+        imgSrc: './image/Submarine 2/Cheese Submarine.png',
         unitPrice: 2200.00,
         discount: '-',
         netPrice: 2200.00
@@ -222,7 +222,7 @@ function B1022() {
     addBurger({
         id: 'B1022',
         name: 'Double Cheese n Chicken Submarine',
-        imgSrc: '/image/Submarine 2/Double Cheese n Chicken Submarine.png',
+        imgSrc: './image/Submarine 2/Double Cheese n Chicken Submarine.png',
         unitPrice: 1900.00,
         discount: '16%',
         netPrice: 1596.00
@@ -233,7 +233,7 @@ function B1023() {
     addBurger({
         id: 'B1023',
         name: 'Special Horgie Submarine',
-        imgSrc: '/image/Submarine 2/Special Horgie Submarine.png',
+        imgSrc: './image/Submarine 2/Special Horgie Submarine.png',
         unitPrice: 2800.00,
         discount: '-',
         netPrice: 2800.00
@@ -244,7 +244,7 @@ function B1024() {
     addBurger({
         id: 'B1024',
         name: 'MOS Special Submarine',
-        imgSrc: '/image/Submarine 2/MOS Special Submarine.png',
+        imgSrc: './image/Submarine 2/MOS Special Submarine.png',
         unitPrice: 3000.00,
         discount: '-',
         netPrice: 3000.00
@@ -255,7 +255,7 @@ function B1025() {
     addBurger({
         id: 'B1025',
         name: 'Steak Fries (Large)',
-        imgSrc: '/image/Fries/Steak Fries (Large) .png',
+        imgSrc: './image/Fries/Steak Fries (Large) .png',
         unitPrice: 1200.00,
         discount: '-',
         netPrice: 1200.00
@@ -266,7 +266,7 @@ function B1026() {
     addBurger({
         id: 'B1026',
         name: 'Steak Fries (Medium)',
-        imgSrc: '/image/Fries/Steak Fries (Medium).png',
+        imgSrc: './image/Fries/Steak Fries (Medium).png',
         unitPrice: 600.00,
         discount: '-',
         netPrice: 600.00
@@ -277,7 +277,7 @@ function B1027() {
     addBurger({
         id: 'B1027',
         name: 'French Fries (Large)',
-        imgSrc: '/image/Fries/French Fries (Large).png',
+        imgSrc: './image/Fries/French Fries (Large).png',
         unitPrice: 800.00,
         discount: '-',
         netPrice: 800.00
@@ -288,7 +288,7 @@ function B1028() {
     addBurger({
         id: 'B1028',
         name: 'French Fries (Medium)',
-        imgSrc: '/image/Fries/French Fries (Medium).png',
+        imgSrc: './image/Fries/French Fries (Medium).png',
         unitPrice: 650.00,
         discount: '-',
         netPrice: 650.00
@@ -299,7 +299,7 @@ function B1029() {
     addBurger({
         id: 'B1029',
         name: 'French Fries (Small)',
-        imgSrc: '/image/Fries/French Fries (Small).png',
+        imgSrc: './image/Fries/French Fries (Small).png',
         unitPrice: 450.00,
         discount: '-',
         netPrice: 450.00
@@ -310,7 +310,7 @@ function B1030() {
     addBurger({
         id: 'B1030',
         name: 'Sweet Potato Fries (Large)',
-        imgSrc: '/image/Fries/Sweet Potato Fries (Large).png',
+        imgSrc: './image/Fries/Sweet Potato Fries (Large).png',
         unitPrice: 600.00,
         discount: '-',
         netPrice: 600.00
@@ -321,7 +321,7 @@ function B1031() {
     addBurger({
         id: 'B1031',
         name: 'Chicken n Cheese Pasta',
-        imgSrc: '/image/Pasta/Chicken n Cheese Pasta.png',
+        imgSrc: './image/Pasta/Chicken n Cheese Pasta.png',
         unitPrice: 1600.00,
         discount: '15%',
         netPrice: 1360.00
@@ -332,7 +332,7 @@ function B1032() {
     addBurger({
         id: 'B1032',
         name: 'Chicken Penne Pasta',
-        imgSrc: '/image/Pasta/Chicken Penne Pasta.png',
+        imgSrc: './image/Pasta/Chicken Penne Pasta.png',
         unitPrice: 1700.00,
         discount: '15%',
         netPrice: 1700.00
@@ -343,7 +343,7 @@ function B1033() {
     addBurger({
         id: 'B1033',
         name: 'Ground Turkey Pasta Bake',
-        imgSrc: '/image/Pasta/Ground Turkey Pasta Bake.png',
+        imgSrc: './image/Pasta/Ground Turkey Pasta Bake.png',
         unitPrice: 2900.00,
         discount: '10%',
         netPrice: 2610.00
@@ -354,7 +354,7 @@ function B1034() {
     addBurger({
         id: 'B1034',
         name: 'Creamy Shrimp Pasta',
-        imgSrc: '/image/Pasta/Creamy Shrimp Pasta.png',
+        imgSrc: './image/Pasta/Creamy Shrimp Pasta.png',
         unitPrice: 2000.00,
         discount: '-',
         netPrice: 2000.00
@@ -365,7 +365,7 @@ function B1035() {
     addBurger({
         id: 'B1035',
         name: 'Lemon Butter Pasta',
-        imgSrc: '/image/Pasta/Lemon Butter Pasta.png',
+        imgSrc: './image/Pasta/Lemon Butter Pasta.png',
         unitPrice: 1950.00,
         discount: '-',
         netPrice: 1950.00
@@ -376,7 +376,7 @@ function B1036() {
     addBurger({
         id: 'B1036',
         name: 'Tagliatelle Pasta',
-        imgSrc: '/image/Pasta/Tagliatelle Pasta.png',
+        imgSrc: './image/Pasta/Tagliatelle Pasta.png',
         unitPrice: 2400.00,
         discount: '1%',
         netPrice: 2376.00
@@ -387,7 +387,7 @@ function B1037() {
     addBurger({
         id: 'B1037',
         name: 'Baked Ravioli',
-        imgSrc: '/image/Pasta/Baked Ravioli.png',
+        imgSrc: './image/Pasta/Baked Ravioli.png',
         unitPrice: 2000.00,
         discount: '1%',
         netPrice: 1980.00
@@ -398,7 +398,7 @@ function B1038() {
     addBurger({
         id: 'B1038',
         name: 'Fried Chicken (Small)',
-        imgSrc: '/image/Chicken/Fried Chicken (Small).png',
+        imgSrc: './image/Chicken/Fried Chicken (Small).png',
         unitPrice: 1200.00,
         discount: '-',
         netPrice: 1200.00
@@ -409,7 +409,7 @@ function B1039() {
     addBurger({
         id: 'B1039',
         name: 'Fried Chicken (Regular)',
-        imgSrc: '/image/Chicken/Fried Chicken (Regular).png',
+        imgSrc: './image/Chicken/Fried Chicken (Regular).png',
         unitPrice: 2300.00,
         discount: '10%',
         netPrice: 2070.00
@@ -420,7 +420,7 @@ function B1040() {
     addBurger({
         id: 'B1040',
         name: 'Fried Chicken (Large)',
-        imgSrc: '/image/Chicken/Fried Chicken (Large).png',
+        imgSrc: './image/Chicken/Fried Chicken (Large).png',
         unitPrice: 3100.00,
         discount: '5%',
         netPrice: 2945.00
@@ -431,7 +431,7 @@ function B1041() {
     addBurger({
         id: 'B1041',
         name: 'Hot Wings (Large)',
-        imgSrc: '/image/Chicken/Hot Wings (Large) .png',
+        imgSrc: './image/Chicken/Hot Wings (Large) .png',
         unitPrice: 2400.00,
         discount: '-',
         netPrice: 2400.00
@@ -442,7 +442,7 @@ function B1042() {
     addBurger({
         id: 'B1042',
         name: 'Devilled Chicken (Large)',
-        imgSrc: '/image/Chicken/Devilled Chicken (Large).png',
+        imgSrc: './image/Chicken/Devilled Chicken (Large).png',
         unitPrice: 900.00,
         discount: '-',
         netPrice: 900.00
@@ -453,7 +453,7 @@ function B1043() {
     addBurger({
         id: 'B1043',
         name: 'BBQ Chicken (Regular)',
-        imgSrc: '/image/Chicken/BBQ Chicken (Regular) .png',
+        imgSrc: './image/Chicken/BBQ Chicken (Regular) .png',
         unitPrice: 2100.00,
         discount: '-',
         netPrice: 2100.00
@@ -464,7 +464,7 @@ function B1044() {
     addBurger({
         id: 'B1044',
         name: 'Pepsi (330ml)',
-        imgSrc: '/image/Beverages/Pepsi (330ml).png',
+        imgSrc: './image/Beverages/Pepsi (330ml).png',
         unitPrice: 990.00,
         discount: '5%',
         netPrice: 940.50
@@ -475,7 +475,7 @@ function B1045() {
     addBurger({
         id: 'B1045',
         name: 'Coca-Cola (330ml)',
-        imgSrc: '/image/Beverages/Coca-Cola (330ml).png',
+        imgSrc: './image/Beverages/Coca-Cola (330ml).png',
         unitPrice: 1230.00,
         discount: '-',
         netPrice: 1230.00
@@ -486,7 +486,7 @@ function B1046() {
     addBurger({
         id: 'B1046',
         name: 'Sprite (330ml)',
-        imgSrc: '/image/Beverages/Sprite (330ml).png',
+        imgSrc: './image/Beverages/Sprite (330ml).png',
         unitPrice: 1500.00,
         discount: '3%',
         netPrice: 1455.00
@@ -497,7 +497,7 @@ function B1047() {
     addBurger({
         id: 'B1047',
         name: 'Mirinda (330ml)',
-        imgSrc: '/image/Beverages/Mirinda (330ml).png',
+        imgSrc: './image/Beverages/Mirinda (330ml).png',
         unitPrice: 850.00,
         discount: '7%',
         netPrice: 790.50
